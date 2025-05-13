47c35a3f1f6e9efb649f530854f64836d570c217
Byron Wall <byron@byroni.us>
2025-05-12 23:04:39 -0400

    Add a control flow test file

---
+++ src/__tests__/__fixtures__/controlFlows.ts
@@ -0,0 +1,74 @@
+// create sample TS code that contains a lot of control flow
+// try/catch, if/else, for/while, switch/case, etc.
+// try to make it as complex as possible
+// try to make it as realistic as possible
+// try to make it as long as possible
+// try to make it as interesting as possible
+// try to make it as fun as possible
+
+function controlFlows() {
+  try {
+    // try/catch
+  } catch (error) {
+    // catch
+  }
+}
+
+function controlFlows2(b: boolean) {
+  if (b) {
+    // if
+  } else {
+    // else
+  }
+}
+
+function controlFlows3() {
+  for (let i = 0; i < 10; i++) {
+    // for
+  }
+}
+
+function controlFlows4(d: boolean) {
+  while (d) {
+    // while
+  }
+}
+
+function controlFlows5() {
+  switch (true) {
+    case true:
+    // switch
+  }
+}
+
+function controlFlows6() {
+  let a = 1;
+  const b = 2;
+  const c = 3;
+  const d = 4;
+  const e = 5;
+
+  if (a === 1) {
+    a = 2;
+  } else if (a === 2) {
+    // else if
+  } else {
+    // else
+  }
+}
+
+// more switch cases with different types
+function controlFlows7(a: number | string) {
+  switch (a) {
+    case 1:
+      // switch
+      break;
+    case 2:
+    case 3:
+      // switch
+      break;
+    default:
+      // switch
+      break;
+  }
+}
